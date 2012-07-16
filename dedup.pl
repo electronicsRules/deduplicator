c5753ce27c653a59a611d85234e2cfd10200d371
Gosha T <gosha.tugai@gmail.com>
2012-07-16 17:35:26 +0930

    Progress has been made on option architecture

--- dedup.pl
+++ dedup.pl
@@ -1,17 +1,67 @@
 use Getopt::Long;
 use Digest;
 use Pod::Usage;
-our @hashes=keys %Digest::MMAP;
+our %ht=qw(
+crc16 CRC-16
+crc32 CRC-32
+ripemd RIPEMD-160
+ripemd160 RIPEMD-160
+md5 MD5
+sha SHA-1
+sha1 SHA-1
+sha224 SHA-224
+sha256 SHA-256
+sha384 SHA-384
+sha512 SHA-512
+);
 our %o;
+$o{'collapse'}=1;
+$o{'dedup'}=1;
+$o{'prune-size'}=1;
+$o{'hash'}='md5';
+$o{'quiet'}=2;
+$o{'hash-fixed'}='64k';
+$o{'hash-dyna'}=-1;
+$o{'hash-blocks'}=1;
 GetOptions(\%o,
     'help' => sub {pod2usage(1)},
     'man|manual' => sub {pod2usage(-exitstatus => 0, -verbose => 2)},
     
-    'dedup',
-    'uniq|unique',
+    'dedup' => sub {$o{'uniq'}=0;$o{'dedup'}=1},
+    'uniq|unique' => sub {$o{'dedup'}=0;$o{'uniq'}=1},
 
-    'apply!'
+    'ignore=s@',
+    'ignore-re=s@',
+
+    'apply|a!' => sub {
+        $o{'apply'}=$_[1];
+        if (!$_[1]) {$o{'quiet'}=0 if $o{'quiet'}==2;}
+    },
+    'quiet|q' => sub {$o{'quiet'}=1;$o{'fatal'}=0},
+    'fatal|f' => sub {$o{'quiet'}=0;$o{'fatal'}=1},
+    'interactive|i' => sub {$o{'quiet'}=0;$o{'fatal'}=0;},
+    
+    'collapse!',
+    'fuzzy-coll=i',
+    'fuzzy-coll-a=i',
+    'fuzzy-coll-b=i',
+    
+    'prune-size!',
+    
+    'hash=s',
+    
+    'hash-fixed:s',
+    'hash-fixed-offset=s',
+    
+    'hash-dyna:f',
+    'hash-dyna-minsize=s',
+    
+    'hash-blocks=i',
+    'hash-block-skip=f'
 ) or pod2usage();
+if ($o{'hash-dyna'}==0) {$o{'hash-dyna'}=5}
+if (!$ARGV[0] || $ARGV[2]) {pod2usage()}
+if (!$ht{$o{'hash'}}) {pod2usage()}
 __END__
 =pod
 
@@ -23,6 +73,8 @@ dedup - Find/Remove duplicate files/folders
 
 B<dedup> [B<options>] E<lt>B<folderA>E<gt> [B<folderB>]
 
+B<dedup> [B<options>] E<lt>B<report>E<gt> E<lt>B<folderA>E<gt> [B<folderB>]
+
  Options:
    --help Show a list of options
    --man[ual] Show the documentation
@@ -31,7 +83,32 @@ B<dedup> [B<options>] E<lt>B<folderA>E<gt> [B<folderB>]
    --uniq[ue] Show/copy unique files (from A to B, requires two folders)
 
    --noapply (Default) Report only, do not perform the actions
-   --apply Perform the actions
+   --a[pply] Perform the actions, if a report is given, will use any answers provided in the report. The user will be queried on any ambiguity
+   --q[uiet] (Default if --noapply) If an ambiguity arises, the item is skipped instead of querying the user.
+   --i[nteractive] (Default if --apply) Opposite of --quiet and --fatal
+   --f[atal] If an ambiguity arises, stop
    
+   --ignore=s Wildcard ignore, applies to filenames, can be used multiple times
+   --ignore-re=s Regexp ignore, applies to filenames, can be used multiple times
    
+   --nocollapse
+   --collapse (Default) Show duplicate folders as folders instead of listing all files
+
+   --fuzzy-coll=i (Default 0) Ignore up to N differences between folders when collapsing
+   --fuzzy-coll-a=i (Default 0) Ignore up to N files only in A when collapsing
+   --fuzzy-coll-b=i (Default 0) Ignore up to N files only in B when collapsing
+   
+   --noprune-size
+   --prune-size (Default) Assume files with different sizes are different - can cause problems with broken filesystems
+   
+   --hash=s (Default MD5) Cryptographic hash, one of crc{16,32}, ripemd, sha{,1,224,256,512}
+   
+   --hash-fixed[=s] (Default 64K) Size, in bytes, of the segment to hash (1K = 1024)
+   --hash-fixed-offset=s (Default 0) Offset of the fixed segment (1K = 1024), if the segment goes beyond the end of the file, it will be shifted back to fit
+   
+   --hash-dyna[=f] (Default 5 but disabled) Percentage of the filesize to hash
+   --hash-dyna-minsize=s (Default 8K) Minimum size of dynamic hash segment, if file is smaller than this, whole file is hashed
+   
+   --hash-blocks=i (Disabled by default) Hash N segments of the file, each segment is size/N bytes
+   --hash-block-skip=f (Default 1) Multiplier for skip size
 =cut
